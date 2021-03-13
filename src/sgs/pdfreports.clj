faf3acc0362d005929dd37bd3f7197dce6ebf988
Jacek Podkanski <ruby.object@googlemail.com>
2021-03-13 13:41:04 +0000

    trying again based on working example

--- src/sgs/pdfreports.clj
+++ src/sgs/pdfreports.clj
@@ -1,6 +1,12 @@
 (ns sgs.pdfreports
-  (:gen-class)
-  (:require [clojure.data.xml :as xml]))
+  (:require [clojure.data.xml :as xml]
+            [tupelo.core :as tup]))
+
+
+;;; terminal use
+;; (require 'sgs.pdfreports)
+;; (ns sgs.pdfreports)
+;; (tryme)
 
 (defn greet
   "Callable entry point to the application."
@@ -15,13 +21,15 @@
 ;; (.tag (tryme))
 ;; (.attrs (tryme))
 
-sgs.pdfreports> (type (.content (tryme)))
+;sgs.pdfreports> (type (.content (tryme)))
 
 (defn tryme
   "Try parsing a string."
   []
   (let [str (slurp "/home/jacek/Desktop/Reports/1345098")]
-    (xml/parse-str str)))
+    (xml/parse-str str))
+
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
